66cc6bf0097a5fa7b333efee4c7275b773910a7e
PrudhviRajMacherla <macharlaprudhvi3@gmail.com>
2025-10-14 13:03:16 +0530

    db level validation api level validtion

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const validator = require('validator');
 
 const { connectDb } = require("../utils/database");
 const User = require("../models/user");
@@ -7,31 +8,65 @@ const User = require("../models/user");
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const data = req.body;
+    try{
+      
+      const arr = ['firstName','lastName','email','password'];
 
-  const { firstName, lastName, AGE } = req.body;
+      for (let x in req.body) {
+        if(!arr.includes(x))
+        {
+          throw new Error('unneccessary data is being sent')
+        }
+      }
 
-  const user = new User({
-    firstName: firstName,
-    lastName: lastName,
-    age: AGE,
-  });
-
-  await user.save();
-
-  res.send(user);
+      
+      let {firstName,lastName,email,password} = req.body;
+      if(!validator.isEmail(email)){
+        throw new Error('email wrong api level validation')
+      }
+      console.log(req.body);
+      firstName = firstName.trim();
+      if(firstName.length>0||password){
+        console.log(firstName);
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).send(user);
+      }
+      else{
+        throw new Error('Make sure you enter a valid name');
+      }
+    }
+    catch(err){
+     
+      res.status(500).send('something went wrong'+err)
+    }
 });
 
-app.get("/user", async (req, res) => {
+app.get("/user/:name", async (req, res) => {
   try {
-    const usersData = await User.find({ firstName: "prudhvi",lastName:"raj"});
-    res.send(usersData);
+    const {name}= req.params;
+    const singleUser = await User.findOne({firstName:name});
+    if(!singleUser){
+      res.send('user not found');
+    }
+
+    else{
+
+      res.status(200).send(singleUser);
+    }
+
+     
   } catch (err) {
     res.status(500).send("something went wrong");
   }
 });
 
-connectDb();
-app.listen(4321, () => {
-  console.log("Server is running......");
-});
+connectDb().
+then(()=>{
+  app.listen(4321,()=>{
+    console.log('server is started')
+  })
+})
+.catch((err)=>{
+  console.log('something went wrong unable to start server.....'+err.message)
+})
