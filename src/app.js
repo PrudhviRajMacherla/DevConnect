2b02f969fb5f6603bb8c2e5630a4c138c10903a4
PrudhviRajMacherla <macharlaprudhvi3@gmail.com>
2025-10-22 13:06:12 +0530

    jwt token verification

--- src/app.js
+++ src/app.js
@@ -1,84 +1,95 @@
 const express = require("express");
 const app = express();
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const validator = require("validator");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
-const {validateSignup} = require('../utils/validateSignup');
+const { validateSignup } = require("../utils/validateSignup");
 const { connectDb } = require("../utils/database");
 const User = require("../models/user");
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.post("/signup", async (req, res) => {
+app.post("/register", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
 
-  const {firstName,lastName,email,password} = req.body;
-  
-  try{
-     validateSignup(req);
-     const previousData = await User.findOne({email:email});
-     
-     if(previousData){
-      throw new Error("User is already Exists ")
-     }
-     
-     const hashedPassword =await bcrypt.hash(password,10);
-     const newUser = new User({
+  try {
+    validateSignup(req);
+    const previousData = await User.findOne({ email: email });
+
+    if (previousData) {
+      throw new Error("User is already Exists ");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
       firstName,
       lastName,
       email,
-      password :hashedPassword
-     })
-     await newUser.save();
-     res.status(201).send(newUser)
-     
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(201).send(newUser);
+  } catch (error) {
+    res.status(500).send(error.message);
   }
-  catch(error){
-    res.status(500).send(error.message)
-  }
-   
-
 });
 
-
-app.post("/login",async(req,res)=>{
-  try{
- 
-
-   const {email,password} = req.body;
-   const validuser = await User.findOne({email:email});
-   console.log(validuser); 
-
-    if(!validuser){
-      throw new error('user was not found please Signup');
-    }
-
-    const isvalidpassword =  await bcrypt.compare(password,validuser.password);
-
-    if(!isvalidpassword)
-    {
-      throw new Error('Password Not matched');
-    }
-
-    res.send('token');
-
-
-  }
-  catch(error){
-      res.status(500).send(error.message);
-  }
-})
-
-app.get("/user/:name", async (req, res) => {
+app.post("/login", async (req, res) => {
   try {
-    const { name } = req.params;
-    const singleUser = await User.findOne({ firstName: name });
-    if (!singleUser) {
-      res.send("user not found");
-    } else {
-      res.status(200).send(singleUser);
+    console.log("line 46");
+    const { email, password } = req.body;
+    const userFound = await User.findOne({ email: email });
+
+    if (!userFound) {
+      throw new Error("Register First to login");
     }
-  } catch (err) {
-    res.status(500).send("something went wrong");
+
+    const isvalidpassword = await bcrypt.compare(password, userFound.password);
+
+    if (isvalidpassword) {
+      const payload = {
+        _id: userFound._id,
+      };
+
+      const token = await jwt.sign(payload, "DevConnect@123", {
+        expiresIn: "5m"
+      });
+
+      res.cookie("access_token", token);
+      res.send("token sent sucessfully after login");
+    } else {
+      console.log("line 65");
+      throw new Error("Invalid Credentials....");
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.get("/profile", async (req, res) => {
+  try {
+    const { access_token } = req.cookies;
+    console.log("line 93", access_token);
+
+    // const decoded = await jwt.verify(access_token,"DevConnect@123");
+    // console.log(decoded);
+
+    const decoded = jwt.verify(access_token, "DevConnect@123");
+    console.log("✅ Token verified:", decoded);
+
+    // If token is valid, send user data
+    const {_id}= decoded;
+    const getUserData = await User.findById(_id);
+    res.status(200).send({
+      user: getUserData, // or fetch user from DB using decoded.id
+    });
+  } catch (err) 
+  {
+    return res.status(500).json({ message: "Internal server error" });
+    
   }
 });
 
